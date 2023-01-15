195fea75da4279b696d74df6aa3bda8b5e6d9515
itsspriyansh <priyanshkp01@gmail.com>
2023-01-16 03:54:25 +0530

    commited

---
+++ client/src/components/CityItem.jsx
@@ -0,0 +1,15 @@
+function CityItem () {
+    return (
+        <div className=" w-72 h-72 bg-green-300">
+            <div className=" w-2/4">
+                {/* <image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz-BFncHfJq5OEmFnOAnrO9oSoR3Yx-75bdTzNmxdPEw&s" /> */}
+            </div>
+            <div>
+                <p>Hello There</p>
+            </div>
+        </div>
+    )
+}
+
+export default CityItem
+
