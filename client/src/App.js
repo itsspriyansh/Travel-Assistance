467dcbfa1be5d5ac614ce9f34b410e32dc63cd18
itsspriyansh <priyanshkp01@gmail.com>
2023-01-16 11:05:58 +0530

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,15 @@
-import Carousel from "./components/Carousel"
-import Header from './components/Header';
 import './App.css';
-import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+import CityPage from "./pages/CityPage";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
-      <div className=" h-20 items-center flex justify-center bg-gray-300">Explore</div>
-      <main className=" bg-gray-300">
-        <div className=" mb-10 py-10 px-20 bg-white"> 
-        <p className="flex justify-center">Heritage Sites</p>
-          <Carousel /> 
-        </div>
-        <div className=" mb-10 py-10 px-20 bg-white"> 
-        <p className="flex justify-center">Heritage Sites</p>
-          <Carousel /> 
-        </div>
-        <div className=" mb-10 py-10 px-20 bg-white"> 
-        <p className="flex justify-center">Heritage Sites</p>
-          <Carousel /> 
-        </div>
-      </main>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/city-info" element={<CityPage />} />
+      </Routes>
     </div>
   );
 }
