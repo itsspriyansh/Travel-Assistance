e5efb7b1c80a5916ee6e36777ba00352de87744f
itsspriyansh <priyanshkp01@gmail.com>
2023-01-16 05:56:01 +0530

    added cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import Carousel from "./UI/Carousel"
+import Carousel from "./components/Carousel"
 import Header from './components/Header';
 import './App.css';
+import CityItem from "./components/CityItem";
 
 function App() {
   return (
     <div>
       <Header />
-      <Carousel />
+      <div className="mb-10">Explore</div>
+      <main className=" w-11/12 mx-auto">
+        <Carousel />
+        {/* <CityItem /> */}
+      </main>
     </div>
   );
 }
 
 export default App;
+
--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import CityItem from "../components/CityItem";
+import CityItem from "./CityItem";
 
 export default class AutoPlay extends Component {
   render() {
     const settings = {
       dots: true,
       infinite: true,
-      slidesToShow: 3,
+      slidesToShow: 5,
       slidesToScroll: 1,
       autoplay: true,
       speed: 2000,
-      autoplaySpeed: 1000,
+      autoplaySpeed: 2000,
       cssEase: "linear"
     };
     return (
       <div>
         <Slider {...settings}>
-          <div>
+          <div className=" m-2">
             <CityItem />
           </div>
           <div>
@@ -40,3 +40,4 @@ export default class AutoPlay extends Component {
     );
   }
 }
+
